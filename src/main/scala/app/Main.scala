959f72a58bcc7ccdbd2ce2ff541de4a771c87bf8
Dominik Frey <frey@brickwork.ch>
2023-04-05 11:43:47 +0200

    patch and analytics

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,6 +1,8 @@
 package app
 
 import app._
+import app.analytics.SimpleAnalytics
+import app.loaders.{MoviesLoader, RatingsLoader}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Main {
@@ -9,5 +11,13 @@ object Main {
     val sc = SparkContext.getOrCreate(conf)
 
     //your code goes here
+    val moviesLoader = new MoviesLoader(sc, "./src/main/resources/movies_small.csv")
+    val ratingsLoader = new RatingsLoader(sc, "./src/main/resources/ratings_small.csv")
+
+    val movies = moviesLoader.load()
+    val ratings = ratingsLoader.load()
+
+    val simpleAnalytics = new SimpleAnalytics
+    simpleAnalytics.init(ratings, movies)
   }
 }
