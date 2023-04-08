7a01aabfe0942bd4bd9f7b0fcccbfec8a5743e4b
Dominik Frey <frey@brickwork.ch>
2023-04-08 13:59:35 +0200

    milestone 2

--- src/main/scala/app/aggregator/Aggregator.scala
+++ src/main/scala/app/aggregator/Aggregator.scala
@@ -12,7 +12,8 @@ import org.apache.spark.storage.StorageLevel.MEMORY_AND_DISK
 class Aggregator(sc: SparkContext) extends Serializable {
 
   private var state = null
-  private var partitioner: HashPartitioner = null
+  private var partitioner: HashPartitioner = new HashPartitioner(100)
+  private var avgRatings: RDD[(Int, String, Double, Int, List[String])] = null
 
   /**
    * Use the initial ratings and titles to compute the average rating for each title.
@@ -25,14 +26,35 @@ class Aggregator(sc: SparkContext) extends Serializable {
   def init(
             ratings: RDD[(Int, Int, Option[Double], Double, Int)],
             title: RDD[(Int, String, List[String])]
-          ): Unit = ???
+          ): Unit = {
+    val movies =
+      title
+        .groupBy(_._1)
+        .map(movie => (movie._1, movie._2.head))
+
+    avgRatings =
+      ratings
+        .groupBy(rating => (rating._1, rating._2))
+        .map(rating => (rating._1._2, rating._2.maxBy(_._5)._4))
+        .groupByKey()
+        .partitionBy(partitioner)
+        .map(ratings => (ratings._1, (ratings._2.sum/ratings._2.size, ratings._2.size)))
+        .rightOuterJoin(movies)
+        .map(avgRatings =>
+          (avgRatings._1, avgRatings._2._2._2, avgRatings._2._1.map(_._1).getOrElse(0.0), avgRatings._2._1.map(_._2).getOrElse(0), avgRatings._2._2._3))
+
+    avgRatings.persist()
+  }
 
   /**
    * Return pre-computed title-rating pairs.
    *
    * @return The pairs of titles and ratings
    */
-  def getResult(): RDD[(String, Double)] = ???
+  def getResult(): RDD[(String, Double)] = {
+    avgRatings
+      .map(rating => (rating._2, rating._3))
+  }
 
   /**
    * Compute the average rating across all (rated titles) that contain the
@@ -43,7 +65,26 @@ class Aggregator(sc: SparkContext) extends Serializable {
    * @return The average rating for the given keywords. Return 0.0 if no
    *         such titles are rated and -1.0 if no such titles exist.
    */
-  def getKeywordQueryResult(keywords: List[String]): Double = ???
+  def getKeywordQueryResult(keywords: List[String]): Double = {
+    val filteredByKeywords =
+      avgRatings
+      .filter(movie =>
+        keywords.forall(movie._5.contains))
+
+    if (filteredByKeywords.isEmpty())
+      return -1.0
+
+    val rated =
+      filteredByKeywords
+      .filter(movie => movie._3 != 0.0)
+
+    if (rated.isEmpty())
+      0.0
+    else
+      rated
+        .map(movie => movie._3)
+        .mean()
+  }
 
   /**
    * Use the "delta"-ratings to incrementally maintain the aggregate ratings
@@ -51,5 +92,40 @@ class Aggregator(sc: SparkContext) extends Serializable {
    *  @param delta Delta ratings that haven't been included previously in aggregates
    *        format: (user_id: Int, title_id: Int, old_rating: Option[Double], rating: Double, timestamp: Int)
    */
-  def updateResult(delta_ : Array[(Int, Int, Option[Double], Double, Int)]): Unit = ???
+  def updateResult(delta_ : Array[(Int, Int, Option[Double], Double, Int)]): Unit = {
+    val deltaGroupedByMovie =
+      sc.parallelize(delta_)
+        .groupBy(rating => (rating._1, rating._2))
+        .map(rating => (rating._1._2, rating._2.maxBy(_._5)))
+
+    val deltaGroupedWithNewCounts =
+      deltaGroupedByMovie
+        .groupBy(_._1)
+        .map(ratingsPerMovie =>
+          (ratingsPerMovie._1, ratingsPerMovie._2.map(rating => (rating._2._3.getOrElse(0.0), rating._2._4))))
+        .map(ratings =>
+          (ratings._1, ratings._2.count(_._1 == 0.0), ratings._2))
+
+  val existingJoinedWithDelta =
+    avgRatings
+      .groupBy(_._1)
+      .leftOuterJoin(deltaGroupedWithNewCounts.groupBy(_._1))
+      .map(movie =>
+        (movie._2._1.head,
+          movie._2._2.getOrElse(Iterable((movie._2._1.head._1, 0, Iterable((0.0, 0.0))))).head._2,
+          movie._2._2.getOrElse(Iterable((movie._2._1.head._1, 0, Iterable((0.0, 0.0))))).head._3.map(_._1),
+          movie._2._2.getOrElse(Iterable((movie._2._1.head._1, 0, Iterable((0.0, 0.0))))).head._3.map(_._2)))
+
+  avgRatings =
+      existingJoinedWithDelta
+      .map(movie =>
+        (movie._1._1,
+          movie._1._2,
+          if ((movie._1._4 + movie._2) != 0) (movie._1._3 * movie._1._4 - movie._3.sum + movie._4.sum)/(movie._1._4 + movie._2) else 0.0,
+          movie._1._4 + movie._2,
+          movie._1._5))
+  avgRatings.persist()
+
+  }
+
 }
