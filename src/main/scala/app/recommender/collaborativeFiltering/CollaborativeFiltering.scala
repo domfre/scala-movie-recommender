5485d3fb84e7c7b0c4b477153c8d483fb1df0931
Dominik Frey <frey@brickwork.ch>
2023-04-12 13:48:52 +0200

    CollaborativeFiltering

--- src/main/scala/app/recommender/collaborativeFiltering/CollaborativeFiltering.scala
+++ src/main/scala/app/recommender/collaborativeFiltering/CollaborativeFiltering.scala
@@ -2,6 +2,7 @@ package app.recommender.collaborativeFiltering
 
 
 import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
 
 class CollaborativeFiltering(rank: Int,
                              regularizationParameter: Double,
@@ -10,10 +11,25 @@ class CollaborativeFiltering(rank: Int,
 
   // NOTE: set the parameters according to the project description to get reproducible (deterministic) results.
   private val maxIterations = 20
-  private var model = null
+  private var model: MatrixFactorizationModel = null
 
-  def init(ratingsRDD: RDD[(Int, Int, Option[Double], Double, Int)]): Unit = ???
+  def init(ratingsRDD: RDD[(Int, Int, Option[Double], Double, Int)]): Unit = {
+    val latestRatingsPerUserAndMovie = mapLatestRatingsPerUserAndMovie(ratingsRDD)
+    model =
+      ALS
+        .train(latestRatingsPerUserAndMovie, rank, maxIterations, regularizationParameter, n_parallel, seed)
+  }
 
-  def predict(userId: Int, movieId: Int): Double = ???
+  def predict(userId: Int, movieId: Int): Double = {
+    model.predict(userId, movieId)
+  }
+
+  def mapLatestRatingsPerUserAndMovie(ratingsRDD: RDD[(Int, Int, Option[Double], Double, Int)]): RDD[Rating] = {
+    ratingsRDD
+      .map(rating => ((rating._1, rating._2), rating._3, rating._4, rating._5))
+      .groupBy(_._1)
+      .map(ratingsPerUserAndMovie =>
+        Rating(ratingsPerUserAndMovie._1._1, ratingsPerUserAndMovie._1._2, ratingsPerUserAndMovie._2.maxBy(_._4)._3))
+  }
 
 }
